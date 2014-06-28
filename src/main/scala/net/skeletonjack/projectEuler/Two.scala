5c5cb7d93a17a4fa9b66c681863374a2baa6a46e
Brian T. Grant <brian.grant@morningstar.com>
2014-06-27 22:31:03 -0500

    Completed Problem Two.

---
+++ src/main/scala/net/skeletonjack/projectEuler/Two.scala
@@ -0,0 +1,31 @@
+package net.skeletonjack.projectEuler
+
+object Two {
+
+  def generateFibonacciTerms(stopGeneratingBefore: Int, filter: Int => Boolean = _ => true): List[Int] = {
+
+    def accumulateFibonacciTerms(currentTerm: Int, previousTerm: Int, collectedTerms: List[Int]): List[Int] = {
+      val newTerm = currentTerm + previousTerm
+
+      if (newTerm >= stopGeneratingBefore)
+        collectedTerms
+      else
+        accumulateFibonacciTerms(newTerm, currentTerm, if (filter(newTerm))
+                                                         collectedTerms :+ newTerm
+                                                       else
+                                                         collectedTerms
+        )
+
+    }
+
+    accumulateFibonacciTerms(1, 0, List())
+  }
+
+  def main(args: Array[String]) {
+    val evenFibTerms = generateFibonacciTerms(4000000, _ % 2 == 0)
+    val totalOfEvenFibTerms = evenFibTerms.sum
+
+    println(s"The total of all, even Fibonacci terms less than 4,000,000 is $totalOfEvenFibTerms.")
+  }
+
+}
---
+++ src/test/scala/net/skeletonjack/projectEuler/TwoTest.scala
@@ -0,0 +1,26 @@
+package net.skeletonjack.projectEuler
+
+import org.scalatest.FunSuite
+
+class TwoTest extends FunSuite {
+
+  test("All Fibonacci terms less than 90") {
+    val fibTermsUnder90 = Two.generateFibonacciTerms(90)
+    assert(List(1, 2, 3, 5, 8, 13, 21, 34, 55, 89) === fibTermsUnder90)
+  }
+
+  test("Fibonacci terms less than 90 and divisible by 3") {
+    val fibTermsUnder90DivisibleBy3 = Two.generateFibonacciTerms(90, _ % 3 == 0)
+    assert(List(3, 21) === fibTermsUnder90DivisibleBy3)
+  }
+
+  test("Fibonacci terms less than 90 and are even") {
+    val evenFibTermsUnder90 = Two.generateFibonacciTerms(90, _ % 2 == 0)
+    assert(List(2, 8, 34) === evenFibTermsUnder90)
+  }
+
+  test("The total of all even Fibonacci terms less than 4,000,000 is 4,613,732") {
+    assert(4613732 === Two.generateFibonacciTerms(4000000, _ % 2 == 0).sum)
+  }
+
+}
