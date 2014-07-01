1120db3b8813bc69540c833862129f8be23fbdac
Brian T. Grant <brian.grant@morningstar.com>
2014-07-01 08:17:33 -0500

    Clean-up & refactoring. isPrime now uses recursive helper method rather than var and while.

--- src/main/scala/net/skeletonjack/projectEuler/Three.scala
+++ src/main/scala/net/skeletonjack/projectEuler/Three.scala
@@ -1,64 +1,26 @@
 package net.skeletonjack.projectEuler
 
-// TODO tracking non-primes we've already calculated would speed things up significantly
 object Three {
 
-  private val primes: collection.mutable.Set[Long] = collection.mutable.Set.empty[Long]
-  private val nonPrimes: collection.mutable.Set[Long] = collection.mutable.Set.empty[Long]
-
   def isPrime(number: Long): Boolean = {
-    println("testing primacy of " + number)
-    // TODO optimization; try w/out
-//    if (primes.contains(number)) {
-//      true
-//    } else if (nonPrimes.contains(number)) {
-//      false
-//    } else {
-      if (number == 1) {
-        false
-      } else {
-        // TODO try recursion & val
-        var x = 2L
 
-        while (x < number) {
-          if (number % x == 0) {
-            nonPrimes.add(number)
-            return false
-          }
-
-          x += 1L
-        }
-        primes.add(number)
+    def recursivelySearchForPrimes(x: Long): Boolean = {
+      if (number == x)
         true
-      }
-//    }
+      else if (number % x == 0)
+        false
+      else
+        recursivelySearchForPrimes(x + 1)
+    }
 
-    //      !(2L until number).exists(x => number % x == 0)
+    if (number == 1) {
+      false
+    } else {
+      recursivelySearchForPrimes(2L)
+    }
   }
 
   def calculateFactors(number: Long): List[Long] = {
-//    val possibleFactors = 1L to number // FIXME range won't work with large numbers
-
-    def accumulateFactors(potentialFactor: Long, factors: List[Long]): List[Long] = {
-      if (number == potentialFactor)
-        factors :+ potentialFactor
-      else if (number % potentialFactor == 0)
-        accumulateFactors(potentialFactor + 1, factors :+ potentialFactor)
-      else
-        accumulateFactors(potentialFactor + 1, factors)
-    }
-
-    // FIXME There's a bug here; for each possibleFactor, we need to calculate starting at 1
-//    possibleFactors.foldLeft(List[Long]()) {(factors: List[Long], nextPotentialFactor: Long) =>
-//      if (number % nextPotentialFactor == 0)
-//        factors :+ nextPotentialFactor
-//      else
-//        factors/*.toSet.toList*/
-//    }
-    accumulateFactors(1L, List())
-  }
-
-  def calculateFactorsWithShortCutting(number: Long): List[Long] = {
 
     def accumulateFactors(potentialFactor: Long, factors: List[Long]): List[Long] = {
       if (factors.contains(potentialFactor))
@@ -73,99 +35,16 @@ object Three {
     accumulateFactors(1L, List()).sorted
   }
 
-
-  def calculatePrimeFactors(start: Long, end: Long): List[Long] = {
-    val startTime = System.currentTimeMillis()
-
-    val factors = (start to end)/*.par*/.flatMap{ x: Long => calculateFactors(x) }.toSet.par.filter(isPrime)
-//    val primes = factors.filter(isPrime)
-
-    println(s"elapsed ${System.currentTimeMillis() - startTime} calculating primes for factors between $start and $end")
-
-//    primes.toList.sorted
-    factors.toList.sorted
+  def calculatePrimeFactors(number: Long): List[Long] = {
+    calculateFactors(number).filter(isPrime).toList.sorted
   }
-
-  def findPrimeNumbers(haystack: List[Long]): List[Long] = {
-    haystack.toSet.par.filter(isPrime).toList.sorted
-  }
-
-  def calculatePrimeFactorsOptimized(number: Long): List[Long] = {
-    calculateFactorsWithShortCutting(number).par.filter(isPrime).toList.sorted
-  }
-
-  /*
-
-[info] ThreeTest:
-[info] - 3, 11 & 17 are prime
-6857
-[info] ThreeTest:
-[info] - 3, 11 & 1
-   */
 
   def main(args: Array[String]) {
     val start = System.currentTimeMillis()
-    val theBigNumber = 600851475143L
-    val numberOfGroups: Long = 600851475143L / (Int.MaxValue / 2)
-    val sizeOfGroups = theBigNumber / numberOfGroups + 1
+    val largestPrimeFactor = calculatePrimeFactors(600851475143L).max
 
-    println("number of groups = " + numberOfGroups)
-    println("size of group = " + sizeOfGroups)
-
-    def normalizeNegativeNumbersToOne(num: Long) = {
-      if (num <= 0)
-        1L
-      else
-        num
-    }
-
-    var largestPrimeFactor: Long = -1
-
-    for (i: Long <- 1L to numberOfGroups) {
-      if (largestPrimeFactor == -1) {
-        val end = (theBigNumber - ((i - 1) * sizeOfGroups))
-        val start = normalizeNegativeNumbersToOne((theBigNumber - (i * sizeOfGroups)) + 1)
-        println(start + " to " + end + " with difference " + (end - start))
-        val primeFactors = calculatePrimeFactors(start, end).reverse
-        println(s"calculated ${primeFactors.size} prime factors")
-        if (!primeFactors.isEmpty && primeFactors.head > largestPrimeFactor) {
-          largestPrimeFactor = primeFactors.head
-        }
-      }
-   }
-
-//    val maxInt: Long = Long.box(Int.MaxValue - 3)
-//    val possibleFactors = (theBigNumber - (Int.MaxValue + 200)) to 600851475143L
-//    val some = possibleFactors.takeRight(Int.MaxValue - 1)
-
-//    for (i: Long <- possibleFactors) {
-//      if (isPrime(i)) {
-//        largestPrimeFactor = i
-//      }
-//    }
-
-    /*possibleFactors.dropWhile { p: Long =>
-      if (!isPrime(p))
-        false
-      else {
-        largestPrimeFactor = p
-        true
-      }
-    }*/
-
-    /*possibleFactors.reverse.find { p: Long =>
-      println(p)
-      isPrime(p)
-    }*//*foreach { x: Long =>
-      if (isPrime(x)) {
-       largestPrimeFactor = x
-      }
-    }*/
-
-//    val primeFactors = calculatePrimeFactors(600851475143L).reverse
-//    println(s"Largest, prime factor of 600851475143 is ${primeFactors.head}")
-      println(s"Largest, prime factor of 600851475143 is $largestPrimeFactor")
-      println("completed after " + (System.currentTimeMillis() - start))
+    println(s"Largest, prime factor of 600851475143 is $largestPrimeFactor")
+    println("completed after " + (System.currentTimeMillis() - start))
   }
 
 }
--- src/test/scala/net/skeletonjack/projectEuler/ThreeTest.scala
+++ src/test/scala/net/skeletonjack/projectEuler/ThreeTest.scala
@@ -18,63 +18,30 @@ class ThreeTest extends FunSuite {
   test("1 & 3 are factors of 3") {
     val factorsOf3 = List(1, 3)
     assert(factorsOf3 === Three.calculateFactors(3))
-    assert(factorsOf3 === Three.calculateFactorsWithShortCutting(3))
   }
 
   test("1, 2, 4 & 8 are factors of 8") {
     val factorsOf8 = List(1, 2, 4, 8)
-
     assert(factorsOf8 === Three.calculateFactors(8))
-    assert(factorsOf8 === Three.calculateFactorsWithShortCutting(8))
   }
 
   test("1, 3, 5 & 15 are factors of 15") {
     val factorsOf15 = List(1, 3, 5, 15)
-
     assert(factorsOf15 === Three.calculateFactors(15))
-    assert(factorsOf15 === Three.calculateFactorsWithShortCutting(15))
+  }
+
+  test("factors of 600851475143") {
+    val factors = List(1, 71, 839, 1471, 6857, 59569, 104441, 486847, 1234169, 5753023, 10086647, 87625999, 408464633, 716151937, 8462696833L, 600851475143L)
+    assert(factors === Three.calculateFactors(600851475143L))
   }
 
   test("The prime factors of 13195 are 5, 7, 13 and 29") {
     val primeFactorsOf13195 = List(5, 7, 13, 29)
-
-    assert(primeFactorsOf13195 === Three.calculatePrimeFactors(13195, 13195))
-    assert(primeFactorsOf13195 === Three.calculatePrimeFactorsOptimized(13195))
+    assert(primeFactorsOf13195 === Three.calculatePrimeFactors(13195))
   }
 
-  ignore("577204368781 is prime") {
-    assert(Three.isPrime(577204368781L))
+  test("SOLUTION:  The largest, prime factor of 600851475143 is 6857") {
+    assert(6857 === Three.calculatePrimeFactors(600851475143L).max)
   }
-
-  ignore("Stuff") {
-    val primeFactors = Three.calculatePrimeFactors(577204368780L, 577204368781L)
-    println(primeFactors.size)
-  }
-
-  ignore("do the math") {
-    val primeFactors = Three.calculatePrimeFactors(600851475143L, 600851475143L)
-    println(primeFactors.reverse.head)
-  }
-
-  test("factors of the big number") {
-    val start = System.currentTimeMillis()
-//    val factors = Three.calculateFactors(600851475143L)
-    val middle = System.currentTimeMillis()
-    val quickerFactors = Three.calculateFactorsWithShortCutting(600851475143L)
-    val end = System.currentTimeMillis()
-    println(s"quick calculation produced $quickerFactors")
-
-    println(s"factor quick calculation completed in ${(end - middle) / 1000} seconds")
-  }
-
-  test("A simple plan") {
-    val start = System.currentTimeMillis()
-    val factorsOfTheBigNumber = Three.calculateFactorsWithShortCutting(600851475143L)
-
-    val primeFactorsOfTheBigNumber = Three.calculatePrimeFactorsOptimized(600851475143L)
-    val largestPrimeFactor = primeFactorsOfTheBigNumber.max
-    println(s"the largest prime factor of 600851475143 is $largestPrimeFactor")
-   }
-
 
 }
